dba8ac0968418c74afaf34e73d8f8332368990fe
sarderibrahim <77164653+sarderibrahim@users.noreply.github.com>
2022-10-23 18:42:26 +0200

    Add files via upload

---
+++ frontend/js/confirmation.js
@@ -0,0 +1,5 @@
+
+
+/* Extracting order Id from document URL and inserting it as text in the orderId HTML element */
+let params = new URL(document.location).searchParams;
+document.getElementById("orderId").textContent = params.get("id");
